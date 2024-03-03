969e9aa41702eb669c97b1af065b2a2a7f8f1680
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-03-03 18:14:29 +0530

    props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,18 +1,22 @@
+import {  } from 'react'
 import './App.css'
-import Card from './components/Card'
+import  Card  from './components/Card'
 
 function App() {
   let myObj = {
-    username: "Sonu",
-    age:23
+    username: "Abhijeet",
+    age: 21
   }
+  let newArr = [1, 2, 3]
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card naam = "abhijeet"/>
-      <Card />
-      <Card />
+     <h1 className='bg-green-400 text-black p-4 
+     rounded-xl mb-4'>Tailwind Test</h1>
+     <Card username="Card" btnText = "Click Me" />
+     <Card username= "Abhi" btnText = "Viste Me" />
+     <Card username= 'Kumar' />
+
     </>
   )
 }
