46188864eed6a4863eb6f6aa347e06fd121aaf2b
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-03-21 20:17:36 +0530

    reactRouter

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React router</h1>
+      
     </>
   )
 }
--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  } from "module";
+import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
