0846a78f4d7c600256d205366f0af29ef4a1049d
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-04-27 20:19:01 +0530

    blog

--- AppwriteProject/src/components/container/container.jsx
+++ AppwriteProject/src/components/container/container.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 
-function container() {
-  return (
-    <div>container</div>
-  )
+function container({children}) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>
+    {children}</div>
 }
 
 export default container
+// In container we defind the height ,width basiaclly styling properties. 
+// its accept the properties of children
